623c024d44f3cfd518071c56f51cfb8ba23c4766
Philipp Haller <hallerp@gmail.com>
2020-10-02 12:56:36 +0200

    Add sample unit test

---
+++ src/test/java/se/sdaproject/ExampleTests.java
@@ -0,0 +1,14 @@
+package se.sdaproject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTests {
+
+    @Test
+    void addsTwoNumbers() {
+	assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }
+
+}
